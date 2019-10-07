2fcd4fa016e2ef5857977eb4f0d220a4ea09d431
QnYosa <55686931+QnYosa@users.noreply.github.com>
2019-10-07 23:28:54 +0200

    Add files via upload

---
+++ 06_pig_latin_spec.rb
@@ -0,0 +1,10 @@
+def translate (a)
+    #prend la première voyelle qu'il voit et met ce qu'il y a avant à la fin et ajoute ay 
+    #length mot - length consonnes result = switch place
+
+    #remove until "AEIO" 
+    #delete until "aeio"
+    #hello".partition("l")  
+a.split.
+    <<
+end
