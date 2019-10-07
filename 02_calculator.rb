fbca9895d7c0f3a5ca947d252b90f1929bfafdcb
QnYosa <55686931+QnYosa@users.noreply.github.com>
2019-10-07 23:26:54 +0200

    Add files via upload

---
+++ 02_calculator.rb
@@ -0,0 +1,23 @@
+def add (nombre_a, nombre_b)
+    nombre_a + nombre_b
+end
+
+def subtract (n_a, n_b)
+    n_a - n_b 
+end 
+
+def sum (array)
+    array.sum 
+end
+
+def multiply (m_a,m_b)
+     m_a * m_b
+end
+
+def power (p_a,p_b)
+    p_a ** p_b
+end
+
+def factorial (f_a)
+    Math.gamma(f_a+1)
+end
